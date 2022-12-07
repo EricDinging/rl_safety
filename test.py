0e1580993b7d6c8210a64bc0b99f03e60a28e50e
EricDinging <ericdingg16@outlook.com>
2022-12-07 00:05:48 -0500

    reboot

--- test.py
+++ test.py
@@ -3,10 +3,10 @@
 
 import gym
 
-env = gym.make("Taxi-v3", render_mode="rgb_array").env
+env = gym.make("Taxi-v3", render_mode="rgb_array")
 
 env.reset()
 env.render()
 
-print("Action Space {}".format(env.action_space))
-print("State Space {}".format(env.observation_space))
+# print("Action Space {}".format(env.action_space))
+# print("State Space {}".format(env.observation_space))
